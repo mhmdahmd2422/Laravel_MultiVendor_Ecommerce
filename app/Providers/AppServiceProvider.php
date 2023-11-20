608d62f04ff13d4d80936eb9e05315dfccb70d85
Mohamed Ahmed <mhmdahmd2422@gmail.com>
2023-11-21 00:21:53 +0200

    update production bug in composer.json

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,24 +26,24 @@ class AppServiceProvider extends ServiceProvider
     {
         Paginator::useBootstrap();
 
-        //start general settings
-        $generalSettings = GeneralSetting::first();
-        $infoSettings = FooterData::first();
-        //Set TimeZone
-        Config::set('app.timezone', $generalSettings->timezone);
-//        //Set App name
-//        Config::set('app.name', $generalSettings->site_name);
-        //share currency to views
-        View::composer('*', function ($view) use ($infoSettings, $generalSettings){
-            $view->with(
-                [
-                    'settings' => $generalSettings,
-                    'info' => $infoSettings,
-                ]
-            );
-        });
-        View::composer(['frontend.dashboard.address.*', 'frontend.pages.checkout'], function ($view){
-            $view->with('countries', config('countries.countries'));
-        });
+//        //start general settings
+//        $generalSettings = GeneralSetting::first();
+//        $infoSettings = FooterData::first();
+//        //Set TimeZone
+//        Config::set('app.timezone', $generalSettings->timezone);
+////        //Set App name
+////        Config::set('app.name', $generalSettings->site_name);
+//        //share currency to views
+//        View::composer('*', function ($view) use ($infoSettings, $generalSettings){
+//            $view->with(
+//                [
+//                    'settings' => $generalSettings,
+//                    'info' => $infoSettings,
+//                ]
+//            );
+//        });
+//        View::composer(['frontend.dashboard.address.*', 'frontend.pages.checkout'], function ($view){
+//            $view->with('countries', config('countries.countries'));
+//        });
     }
 }
